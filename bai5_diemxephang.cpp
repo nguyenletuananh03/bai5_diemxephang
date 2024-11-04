e62cca2d0439abee0c5e7c4e0df099c36a31bd5e
nguyenletuananh03 <phuc98181@gmail.com>
2024-11-04 16:46:12 +0700

    Add project files.

---
+++ bai5_diemxephang.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "chuong trinh diem xep hang\n";
+	double diem;
+	cout << "nhap diem:"; cin >> diem;
+	if (diem < 5)
+		cout << "yeu";
+	else if (diem <= 6)
+		cout << "tb";
+	else if (diem <= 8)
+		cout << "kha";
+	else if (diem > 8)
+		cout << "gioi";
+	return 0;
+}
+
+	
+
+	
+
+	
+
+	
+	
+
+
+	
+	
+
